a144005baf7ab9c0ca079fec9b467eddb5702e5c
Deaw <veerapat231612@gmail.com>
2026-02-16 19:43:26 +0700

    first commit

---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
